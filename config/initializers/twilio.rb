2d50407d906c41b34b3dc09e8b072d0857ac89e4
Stephane Liu <sliu@sjliu.com>
2018-09-19 15:04:59 -1000

    Namespace twilio keys

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -2,6 +2,6 @@ Twilio.configure do |config|
   # config.account_sid = ENV['TWILIO_ACCOUNT_SID']
   # config.auth_toekn  = ENV['TWILIO_AUTH_TOKEN']
 
-  config.account_sid = Rails.application.credentials.twilio_account_sid
-  config.auth_token  = Rails.application.credentials.twilio_auth_token
+  config.account_sid = Rails.application.credentials.twilio[:account_sid]
+  config.auth_token  = Rails.application.credentials.twilio[:auth_token]
 end
