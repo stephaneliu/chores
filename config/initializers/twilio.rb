4fff2918f03cf0c9f2f8142575a1794fca8cbe64
Stephane Liu <sliu@sjliu.com>
2018-09-19 16:22:06 -1000

    Add twilio config on production only

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -2,6 +2,8 @@ Twilio.configure do |config|
   # config.account_sid = ENV['TWILIO_ACCOUNT_SID']
   # config.auth_toekn  = ENV['TWILIO_AUTH_TOKEN']
 
-  config.account_sid = Rails.application.credentials.twilio[:account_sid]
-  config.auth_token  = Rails.application.credentials.twilio[:auth_token]
+  if Rails.env.production?
+    config.account_sid = Rails.application.credentials.twilio[:account_sid]
+    config.auth_token  = Rails.application.credentials.twilio[:auth_token]
+  end
 end
